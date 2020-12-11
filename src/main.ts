99c8bc368aac013cbaf6763136a1c2cb442e1a73
wack17s <keroone17@gmail.com>
2020-12-11 04:00:24 +0200

    fix cors?

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const whitelist = ['https://www.instagram.com', 'https://tattoo-admin.herokuapp.com/'];
+  const whitelist = ['https://www.instagram.com', 'https://tattoo-admin.herokuapp.com', 'http://localhost:3000', 'http://www.instagram.com', 'https://instagram.com', 'http://instagram.com'];
 
   app.use(cors({
     allowedHeaders: [
@@ -14,9 +14,10 @@ async function bootstrap() {
       'X-Parse-Session-Token', 'X-Parse-Application-Id',
     ],
     credentials: true,
+    optionsSuccessStatus: 200,
     methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))
