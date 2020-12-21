325bad190cc185f14999060c3f713a922bb4e7a8
wack17s <keroone17@gmail.com>
2020-12-21 10:08:56 +0200

    disable cors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '30d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const whitelist = ['https://www.instagram.com', 'https://tattoo-admin.herokuapp.com', 'http://localhost:3000', 'http://www.instagram.com', 'https://instagram.com', 'http://instagram.com'];
+  // const whitelist = ['https://tattoo-admin.herokuapp.com', 'http://localhost:3000', 'instagram.com', '*vercel.app'];
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Accept-Encoding', 'Connection',
-      'X-Parse-Session-Token', 'X-Parse-Application-Id',
-    ],
-    credentials: true,
-    optionsSuccessStatus: 200,
-    methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }));
+  // app.use(cors({
+  //   allowedHeaders: [
+  //     'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Accept-Encoding', 'Connection',
+  //     'X-Parse-Session-Token', 'X-Parse-Application-Id',
+  //   ],
+  //   credentials: true,
+  //   optionsSuccessStatus: 200,
+  //   methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
+  //   origin: function (origin, callback) {
+  //     if (whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true)
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'))
+  //     }
+  //   }
+  // }));
 
   await app.listen(process.env.PORT || 3001);
 }
