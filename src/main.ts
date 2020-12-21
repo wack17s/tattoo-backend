1980f874c044675fe02012723b552daa6148bc36
wack17s <keroone17@gmail.com>
2020-12-21 10:17:00 +0200

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,31 @@
 import { NestFactory } from '@nestjs/core';
-// import * as cors from 'cors';
+import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const whitelist = ['https://tattoo-admin.herokuapp.com', 'http://localhost:3000', 'instagram.com', '*vercel.app'];
+  // const whitelist = ['https://tattoo-admin.herokuapp.com', 'https://mytattoo.com.ua'];
 
-  // app.use(cors({
-  //   allowedHeaders: [
-  //     'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Accept-Encoding', 'Connection',
-  //     'X-Parse-Session-Token', 'X-Parse-Application-Id',
-  //   ],
-  //   credentials: true,
-  //   optionsSuccessStatus: 200,
-  //   methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true)
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'))
-  //     }
-  //   }
-  // }));
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Accept-Encoding', 'Connection',
+      'X-Parse-Session-Token', 'X-Parse-Application-Id',
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
+    origin: function (origin, callback) {
+      callback(null, true);
+
+      // if (whitelist.indexOf(origin) !== -1) {
+      //   callback(null, true)
+      // } else {
+      //   callback(new Error('Not allowed by CORS'))
+      // }
+    }
+  }));
 
   await app.listen(process.env.PORT || 3001);
 }
