95bcfceba8c12afa34e7fd27e9d964183e9a6b30
wack17s <keroone17@gmail.com>
2021-02-11 23:21:11 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // const whitelist = ['https://tattoo-admin.herokuapp.com', 'https://mytattoo.com.ua'];
+
   app.use(cors());
 
   await app.listen(process.env.PORT || 3001);
